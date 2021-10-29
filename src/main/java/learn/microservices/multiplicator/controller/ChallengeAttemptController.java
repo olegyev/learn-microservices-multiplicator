bbc800ee53d4ff650a1fc819287961dfdd8cf9d5
olegyev <o.yevstratyev@gmail.com>
2021-10-29 14:31:23 +0300

    Add valid challenge attempt request processing

--- src/main/java/learn/microservices/multiplicator/controller/ChallengeAttemptController.java
+++ src/main/java/learn/microservices/multiplicator/controller/ChallengeAttemptController.java
@@ -1,17 +1,27 @@
 package learn.microservices.multiplicator.controller;
 
+import learn.microservices.multiplicator.dto.ChallengeAttemptDto;
+import learn.microservices.multiplicator.entity.ChallengeAttempt;
 import learn.microservices.multiplicator.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/attempts")
 @RequiredArgsConstructor
 @Slf4j
 public class ChallengeAttemptController {
 
     private final ChallengeService challengeService;
+
+    @PostMapping
+    ResponseEntity<ChallengeAttempt> postResult(@RequestBody ChallengeAttemptDto dto) {
+        return ResponseEntity.ok(challengeService.verifyAttempt(dto));
+    }
 
 }
