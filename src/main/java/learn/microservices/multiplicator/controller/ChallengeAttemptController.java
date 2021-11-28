eecfdf763b38a5dc4ac1607d25ca91eea14ca425
olegyev <o.yevstratyev@gmail.com>
2021-11-28 23:57:40 +0300

    Refactor find challenge attempts by user alias controller

--- src/main/java/learn/microservices/multiplicator/controller/ChallengeAttemptController.java
+++ src/main/java/learn/microservices/multiplicator/controller/ChallengeAttemptController.java
@@ -5,7 +5,6 @@ import learn.microservices.multiplicator.entity.ChallengeAttempt;
 import learn.microservices.multiplicator.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -30,18 +29,8 @@ public class ChallengeAttemptController {
 
     @GetMapping
     public ResponseEntity<List<ChallengeAttempt>> getAllByAlias(@RequestParam String alias,
-                                                                @RequestParam(required = false) Integer page,
-                                                                @RequestParam(required = false) Integer size,
-                                                                @RequestParam(required = false) String sort,
                                                                 @PageableDefault(sort = "timestamp", direction = Sort.Direction.DESC) Pageable defaultPageable) {
-        Pageable pageable;
-        if (page == null || size == null || sort == null) {
-            pageable = defaultPageable;
-        } else {
-            pageable = PageRequest.of(page, size, defaultPageable.getSort());
-        }
-
-        return ResponseEntity.ok(challengeService.findByUserAlias(alias, pageable));
+        return ResponseEntity.ok(challengeService.findByUserAlias(alias, defaultPageable));
     }
 
 }
