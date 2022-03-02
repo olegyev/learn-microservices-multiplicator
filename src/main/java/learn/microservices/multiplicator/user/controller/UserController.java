e990806ad80abc8c3a2d369040f3dc52b4327f4c
olegyev <o.yevstratyev@gmail.com>
2022-03-02 18:29:23 +0300

    Edit comments

--- src/main/java/learn/microservices/multiplicator/user/controller/UserController.java
+++ src/main/java/learn/microservices/multiplicator/user/controller/UserController.java
@@ -3,6 +3,7 @@ package learn.microservices.multiplicator.user.controller;
 import learn.microservices.multiplicator.user.entity.User;
 import learn.microservices.multiplicator.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{ids}")
     public ResponseEntity<List<User>> getUsersByIds(@PathVariable final List<String> ids) {
+        log.info("Get user aliases by IDs {}", ids);
         return ResponseEntity.ok(userService.findAllByIdIn(ids));
     }
 
